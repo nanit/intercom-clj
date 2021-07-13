9b83973c603052d1fd2abe58261d8d5e5a649374
Reut Sharabani <reut.sharabani@gmail.com>
2021-07-13 16:49:28 +0300

    handle api scroll error codes (generic) and add tests

---
+++ test/intercom_clj/users_test.clj
@@ -0,0 +1,24 @@
+(ns intercom-clj.users-test
+  (:require [intercom-clj.users :as users]
+            [clojure.test :refer [is testing deftest]]))
+
+(deftest all-test
+  (testing "success"
+    (testing "no users should return empty collection of users"
+      (with-redefs (users/scroll (constantly {:status 200
+                                              :body   {:users []}}))
+        (is (empty? (users/all)))))
+
+    (testing "scrolling should stop on empty users and return list of users"
+      (with-redefs (users/scroll {nil {:status 200
+                                       :body   {:users        ["user"]
+                                                :scroll_param "1"}}
+                                  "1" {:status 200
+                                       :body   {:users []}}})
+        (is (= ["user"] (users/all))))))
+  (testing "failure"
+    (testing "scrolling should stop and throw an exception on error with no retries"
+      (with-redefs (users/scroll {nil {:status 404
+                                       :body   {:users        ["user"]
+                                                :scroll_param "1"}}})
+        (is (thrown? Exception (users/all)))))))
