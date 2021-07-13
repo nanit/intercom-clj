c5607670000c9b7a2c2b3988337ebb2a129c661f
Reut Sharabani <reut.sharabani@gmail.com>
2021-07-13 16:54:03 +0300

    more tests

--- test/intercom_clj/users_test.clj
+++ test/intercom_clj/users_test.clj
@@ -17,8 +17,16 @@
                                        :body   {:users []}}})
         (is (= ["user"] (users/all))))))
   (testing "failure"
-    (testing "scrolling should stop and throw an exception on error with no retries"
-      (with-redefs (users/scroll {nil {:status 404
+    (testing "failure"
+      (testing "scrolling should stop and throw an exception on error with no retries"
+        (with-redefs (users/scroll {nil {:status 404
+                                         :body   {:users        ["user"]
+                                                  :scroll_param "1"}}})
+          (is (thrown? Exception (users/all))))))
+    (testing "scrolling should stop and throw an exception on error during scrolling with no retries"
+      (with-redefs (users/scroll {nil {:status 200
                                        :body   {:users        ["user"]
-                                                :scroll_param "1"}}})
+                                                :scroll_param "1"}}
+                                  "1" {:status 404
+                                       :body "doesn't matter..."}})
         (is (thrown? Exception (users/all)))))))
