981ef79d4716f19170261cc4ee4cf0ac6f51087d
Reut Sharabani <reut.sharabani@gmail.com>
2021-07-14 09:39:39 +0300

    tests

--- test/intercom_clj/users_test.clj
+++ test/intercom_clj/users_test.clj
@@ -2,6 +2,13 @@
   (:require [intercom-clj.users :as users]
             [clojure.test :refer [is testing deftest]]))
 
+(defn gen-responses [& responses]
+  (let [resps (atom responses)]
+    (fn [_]
+      (let [resp (first @resps)]
+        (swap! resps rest)
+        resp))))
+
 (deftest all-test
   (testing "success"
     (testing "no users should return empty collection of users"
@@ -15,7 +22,19 @@
                                                 :scroll_param "1"}}
                                   "1" {:status 200
                                        :body   {:users []}}})
-        (is (= ["user"] (users/all))))))
+        (is (= ["user"] (users/all)))))
+    (testing "scrolling should return users on error during scrolling with retries more than failures"
+      (with-redefs (users/scroll (gen-responses {:status 200
+                                                 :body   {:users        ["user"]
+                                                          :scroll_param "1"}}
+                                                {:status 404
+                                                 :body   "doesn't matter..."}
+                                                {:status 404
+                                                 :body   "doesn't matter..."}
+                                                {:status 200
+                                                 :body   {:users        []
+                                                          :scroll_param "1"}}))
+        (is (= ["user"] (users/all nil :retries 2))))))
   (testing "failure"
     (testing "failure"
       (testing "scrolling should stop and throw an exception on error with no retries"
@@ -28,5 +47,18 @@
                                        :body   {:users        ["user"]
                                                 :scroll_param "1"}}
                                   "1" {:status 404
-                                       :body "doesn't matter..."}})
-        (is (thrown? Exception (users/all)))))))
+                                       :body   "doesn't matter..."}})
+        (is (thrown? Exception (users/all)))))
+    (testing "scrolling should stop and throw an exception on error during scrolling with retries less than failures"
+      (with-redefs (users/scroll (gen-responses {:status 200
+                                                 :body   {:users        ["user"]
+                                                          :scroll_param "1"}}
+                                                {:status 404
+                                                 :body   "doesn't matter..."}
+                                                {:status 404
+                                                 :body   "doesn't matter..."}
+                                                {:status 404
+                                                 :body   "doesn't matter..."}
+                                                {:status 200
+                                                 :body   {:users []}}))
+        (is (thrown? Exception (users/all :retries 2)))))))
